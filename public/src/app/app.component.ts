2ec53fc094452c9c35518d290287cfb3428cb12c
Vince <32404291+VPYen@users.noreply.github.com>
2018-12-05 18:07:01 -0800

    Update app.component.ts

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpService } from "./http.service"
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap, Params } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'public';
+export class AppComponent implements OnInit {
+  profModal = false;
+  loginModal = true;
+  gameStart = false;
+
+  constructor(private _httpService: HttpService,
+              private _route: ActivatedRoute, private _router: Router) { }
+
+  ngOnInit() {
+    this.profModal = false;
+    this.loginModal = true;
+  }
+
+  toggleProfile() {
+    if(this.profModal == false) {
+      this.profModal = true;
+      this.loginModal = false;
+    }else {
+      this.profModal = false;
+    }
+  }
+
+  closeFromProfile(eventData) {
+    console.log(eventData);
+    this.profModal = eventData;
+  }
+
+  closeFromLogin(eventData) {
+    console.log(eventData);
+    this.loginModal = eventData;
+  }
 }
