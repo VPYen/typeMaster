c94ab168c8003506644b90bb47b657d88781ec24
Vince <32404291+VPYen@users.noreply.github.com>
2018-12-05 19:39:54 -0800

    Update app.component.ts

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Router, ActivatedRoute, ParamMap, Params } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  loginModal = false;
   profModal = false;
-  loginModal = true;
   gameStart = false;
   currentUser = {username: "Guest", avatar: "blankProfileWhite.png"};
 
@@ -17,35 +17,68 @@ export class AppComponent implements OnInit {
               private _route: ActivatedRoute, private _router: Router) { }
 
   ngOnInit() {
+    this.loginModal = false;
     this.profModal = false;
-    this.loginModal = true;
+    this.gameStart = false;
+    this.currentUser = {username: "Guest", avatar: "blankProfileWhite.png"};
+    this.printStatus();
   }
 
   toggleProfile() {
-    if(this.profModal == false) {
+    if (this.profModal == false) {
       this.profModal = true;
       this.loginModal = false;
-    }else {
+      this.gameStart = false;
+
+    }else{
       this.profModal = false;
     }
+    this.printStatus();
+  }
+
+  toggleLogout() {
+    this.currentUser = {username: "Guest", avatar: "blankProfileWhite.png"};
   }
 
   toggleLogin() {
-    if(this.loginModal == false) {
-      this.profModal = false;
+    if (this.loginModal == false) {
       this.loginModal = true;
+      this.profModal = false;
+      this.gameStart = false;
     }else {
       this.loginModal = false;
     }
+    this.printStatus();
+  }
+
+  toggleGame(eventData) {
+    this.gameStart = eventData;
+    this.profModal = false;
+    this.loginModal = false;
+    this.printStatus();
   }
 
   closeFromProfile(eventData) {
-    console.log(eventData);
+    // console.log("event ", eventData);
     this.currentUser = eventData;
+    this.profModal = false;
+    this.loginModal = false;
+    this.printStatus();
   }
 
   closeFromLogin(eventData) {
-    console.log(eventData);
+    // console.log("event ",eventData);
     this.loginModal = eventData;
+    this.profModal = eventData;
+    this.printStatus();
+  }
+
+  setUserFromLogin(eventData) {
+    this.currentUser = eventData;
+    // console.log(this.currentUser);
+  }
+
+  printStatus() {
+    console.log("ProfModal: " + this.profModal + " | LoginModal: " + this.loginModal + " | GameStart: " + this.gameStart);
   }
 }
