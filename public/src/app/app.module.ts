43f6614ecaa057fadc135109409931012e47726d
Vince <32404291+VPYen@users.noreply.github.com>
2018-12-05 18:07:26 -0800

    Update app.module.ts

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpService } from "./http.service"
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ProfileModalComponent } from './profile-modal/profile-modal.component';
+import { LoginRegModalComponent } from './login-reg-modal/login-reg-modal.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileModalComponent,
+    LoginRegModalComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
